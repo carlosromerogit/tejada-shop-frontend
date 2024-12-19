2042cd95a32d8457d6adccbc957feead01568d34
Carlos Romero <169498@listindiario.net>
2024-12-19 10:11:28 -0400

    User dropdown

--- components/HeaderSection.vue
+++ components/HeaderSection.vue
@@ -65,6 +65,26 @@
           color="transparent"
           >Iniciar sesión</UButton
         >
+        <UDropdown :items="dropdownItems" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
+    <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" />
+
+    <template #account="{ item }">
+      <div class="text-left">
+        <p>
+         Iniciaste sesión como
+        </p>
+        <p class="truncate font-medium text-gray-900 dark:text-white">
+          {{ item.label }}
+        </p>
+      </div>
+    </template>
+
+    <template #item="{ item }">
+      <span class="truncate">{{ item.label }}</span>
+
+      <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+    </template>
+  </UDropdown>
 
         <ClientOnly>
           <UButton
@@ -101,6 +121,26 @@ const isDark = computed({
     colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
   },
 });
+const dropdownItems = [
+  [{
+    label: 'ben@example.com',
+    slot: 'account',
+    disabled: true
+  }], [{
+    label: 'Cuenta',
+    icon: 'i-heroicons-cog-8-tooth'
+  }], [{
+    label: 'Órdenes',
+    icon: 'material-symbols-light:orders-outline'
+  }, {
+    label: 'Favoritos',
+    icon: 'material-symbols-light:favorite-outline'
+  }], [{
+    label: 'Cerrar sesión',
+    icon: 'i-heroicons-arrow-left-on-rectangle'
+  }]
+]
+
 </script>
 
 <style scoped></style>
