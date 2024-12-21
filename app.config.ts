dc4d1b3e61673de4cd1a756ecad3b2f40ce6cfe4
Carlos Romero <carlosonasisromero@gmail.com>
2024-12-21 15:04:09 -0400

    Account section

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   ui: {
-    primary:'bg-slate-800',
+    primary: "bg-slate-800",
     button: {
       base: "  ",
       color: {
@@ -18,7 +18,7 @@ export default defineAppConfig({
       color: "text-cyan-500 dark:text-cyan-800",
       ring: "focus-visible:ring-2 focus-visible:ring-slate-900 dark:focus-visible:ring-slate-100 focus-visible:ring-offset-2 focus-visible:ring-offset-white dark:focus-visible:ring-offset-gray-900",
     },
-    input:{
+    input: {
       color: {
         transparent: {
           outline:
@@ -26,13 +26,21 @@ export default defineAppConfig({
         },
       },
     },
-    pagination:{
+    pagination: {
       default: {
-        activeButton:{
-          color:'primary'
-        }
-      }
-    }
+        activeButton: {
+          color: "primary",
+        },
+      },
+    },
+    verticalNavigation: {
+      base: "group relative flex items-center gap-1.5 focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:before:ring-inset focus-visible:before:ring-1 focus-visible:before:ring-stone-900 dark:focus-visible:before:ring-slate-300 before:absolute before:inset-px before:rounded-md disabled:cursor-not-allowed disabled:opacity-75",
+      ring: "focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-stone-900 dark:focus-visible:ring-slate-300",
+      inactive:
+        "text-stone-900 dark:text-slate-300 hover:text-gray-900 dark:hover:text-white hover:before:bg-stone-100 dark:hover:bg-slate-900",
+      icon: {
+        inactive: "text-stone-800 dark:text-slate-300",
+      },
+    },
   },
-
 });
---
+++ pages/cuenta/general.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div>
+      <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex items-baseline justify-between pt-12 pb-2"></div>
+        <section class="pb-8">
+          <div class="flex gap-4">
+            <form class="basis-48 hidden lg:block shrink-0">
+              <div>
+                <UVerticalNavigation :links="links" />
+              </div>
+            </form>
+            <div class="grow">Main</div>
+          </div>
+        </section>
+      </main>
+    </div>
+  </div>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "Tejada Shop | Cuenta",
+});
+definePageMeta({ layout: "default" });
+
+const links = [
+  [
+    {
+      label: "Perfil",
+      icon: "material-symbols:person-2-outline",
+      to: "",
+    },
+    {
+      label: "Mis órdenes",
+      icon: "material-symbols:orders-outline",
+      to: "",
+    },
+    {
+      label: "Direcciones",
+      icon: "material-symbols:directions-alt-outline",
+      to: "",
+    },
+    {
+      label: "Favoritos",
+      icon: "material-symbols:favorite-outline",
+      to: "",
+    },
+  ],
+  [
+    {
+      label: "Cerrar sesión",
+      icon: "material-symbols:logout",
+    },
+  ],
+];
+</script>
+
+<style scoped></style>
